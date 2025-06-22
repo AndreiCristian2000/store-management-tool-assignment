6dc4b5ba95ba1b382e33a6c58e968a00c25cc20b
AndreiCristian2000 <andreihirt@yahoo.com>
2025-06-22 22:12:11 +0300

    add basic auth with roles

--- src/main/java/com/example/storemanagementtoolassignment/controller/ProductController.java
+++ src/main/java/com/example/storemanagementtoolassignment/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.storemanagementtoolassignment.model.Product;
 import com.example.storemanagementtoolassignment.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,21 +16,25 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
         return ResponseEntity.ok(service.getAllProducts());
     }
 
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable Long id) {
         return ResponseEntity.ok(service.getProduct(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
         return ResponseEntity.ok(service.addProduct(product));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}/price")
     public ResponseEntity<Product> updatePrice(@PathVariable Long id, @RequestBody Double price) {
         return ResponseEntity.ok(service.updatePrice(id, price));
