14aae7dcea1bf0d3527e41fe909401718fe80fee
AndreiCristian2000 <andreihirt@yahoo.com>
2025-06-22 15:53:27 +0300

    create spring boot project

---
+++ src/main/java/com/example/storemanagementtoolassignment/StoreManagementToolAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.storemanagementtoolassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreManagementToolAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storemanagementtoolassignment/StoreManagementToolAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storemanagementtoolassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
